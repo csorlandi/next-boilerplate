0ff99933dc8573ac1c36eed64bd727a794bcab2d
Claudio Orlandi <claudiosorlandi@gmail.com>
2022-02-19 03:20:02 -0300

    build(pwa): :sparkles: configure pwa feature

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const withPWA = require('next-pwa');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    disable: !isProduction,
+  },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
