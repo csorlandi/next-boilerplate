7543da51f50da25ccf20e8e978fd0eacc7f80e71
Claudio Orlandi <claudiosorlandi@gmail.com>
2024-12-21 00:06:08 +0100

    feat: set a dynamic favicon

---
+++ src/app/icon.tsx
@@ -0,0 +1,32 @@
+import { ImageResponse } from 'next/og';
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = 'image/png';
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: '32px',
+          background: 'black',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '100%',
+        }}
+      >
+        📦
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
