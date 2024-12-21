63eb5da9adde62de00b97485b8905acf93e388d2
Claudio Orlandi <claudiosorlandi@gmail.com>
2024-12-21 15:55:03 +0100

    chore: change initial screen content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
 import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold">Hello World</h1>
     </div>
   );
 }
